5e664cd2b72c0e2b2a80c8cca30467294a045bd7
최우중 <chwj7700@naver.com>
2023-05-30 22:21:30 +0900

    불꽃수정

--- main/firstFirework.py
+++ main/firstFirework.py
@@ -2,9 +2,10 @@ from firstFireworkParticle import FirstFireworkParticle
 
 class FirstFirework:
     def __init__(self):
-        self.hue = 255
+        self.hue = 42.5
+        self.sat = 255
         self.gravity = PVector(0, 0.2)
-        self.firework = FirstFireworkParticle(width/2-100, height, self.hue, False)
+        self.firework = FirstFireworkParticle(width/2-100, height, self.hue,self.sat, False, 50)
         self.exploded = False
         self.particles = []
         self.count = 0
@@ -28,9 +29,13 @@ class FirstFirework:
         self.count = self.count +1
         
     def explode(self):
-        for _ in range(2000):
-            p = FirstFireworkParticle(self.firework.pos.x, self.firework.pos.y, self.hue, True)
+        for _ in range(1000):
+            p = FirstFireworkParticle(self.firework.pos.x, self.firework.pos.y, 21.25, 0, True, 40)
             self.particles.append(p)
+        
+        for _ in range(1000):
+            p2 = FirstFireworkParticle(self.firework.pos.x, self.firework.pos.y, 42.5, self.sat, True, 30)
+            self.particles.append(p2)
 
     def show(self):
         if not self.exploded:
--- main/firstFireworkParticle.py
+++ main/firstFireworkParticle.py
@@ -1,18 +1,20 @@
 class FirstFireworkParticle:
-    def __init__(self, x, y, hue, exploded):
+    def __init__(self, x, y, hue, sat, exploded, explodedMultval):
         self.pos = PVector(x, y)
         self.prev_positions1 = [self.pos.copy()]  # Store the previous position
         self.prev_positions2 = [self.pos.copy()]  # Store the previous position
         self.exploded = exploded
+        self.explodedMultval = explodedMultval
         self.lifespan = 255
         self.weight = 20
         self.hue = hue
+        self.sat = sat
 
         if not self.exploded:
             self.vel = PVector(3, -15)
         else:
             self.vel = self.random2D()
-            self.vel.mult(random(2, 50))
+            self.vel.mult(random(2, explodedMultval))
 
         self.acc = PVector(0, 0)
 
@@ -26,6 +28,7 @@ class FirstFireworkParticle:
         
         self.pos.x = self.pos.x + random(-1, 1)
         self.prev_positions1.append(self.pos.copy())
+        
         time_scale = 0.3
         self.vel.add(PVector.mult(self.acc, time_scale))
         self.pos.add(PVector.mult(self.vel, time_scale))    
@@ -53,27 +56,25 @@ class FirstFireworkParticle:
                 #self.prev_positions1.pop(0)
     
             strokeWeight(self.weight)
-            stroke(42.5,255,255)
+            stroke(self.hue, self.sat, 255)
             point(self.pos.x, self.pos.y)
         else:
-            strokeWeight(2)  # adjust stroke weight to your needs
-            stroke(42.5, 255, self.lifespan)
+            strokeWeight(1)  # adjust stroke weight to your needs
             
             # Draw lines between all subsequent pairs of previous positions
             for i in range(1, len(self.prev_positions2)):
+                stroke(self.hue, self.sat, (30-i)*(self.lifespan*0.001))
                 line(self.prev_positions2[i-1].x, self.prev_positions2[i-1].y, 
                      self.prev_positions2[i].x, self.prev_positions2[i].y)
-            
-            stroke(0, 0, self.lifespan)
-            for i in range(1, len(self.prev_positions2)):
-                line(self.prev_positions2[i-1].x, self.prev_positions2[i-1].y, 
-                     self.prev_positions2[i].x, self.prev_positions2[i].y)
-            
+                
+            strokeWeight(random(3,5))
+            stroke(self.hue, self.sat, self.lifespan)
+            point(self.pos.x, self.pos.y)
             # Add the current position to the list of previous positions
             self.prev_positions2.append(self.pos.copy())
             
             # If there are more than 10 previous positions, remove the oldest one
-            if len(self.prev_positions2) > 2:
+            if len(self.prev_positions2) > 15:
                 self.prev_positions2.pop(0)
 
     def random2D(self):
