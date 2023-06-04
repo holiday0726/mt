b84dc2450c169f63d0ea8a9412f9c9be8df474fe
DESKTOP-7EHH2PF\chan <holiday@soongsil.ac.kr>
2023-06-04 14:31:12 +0900

    문구 추가

--- main/page1_1.py
+++ main/page1_1.py
@@ -6,23 +6,59 @@ class Page1_1:
     def __init__(self):
         self.stars = []
         self.firework = FirstFirework()
-
-        self.textImage = loadImage("text1.png")
-        
+        self.currentImage = loadImage("text4.png")
         for i in range(30):  # adjust amount of stars here
             self.stars.append(PVector(random(width), random(height)))
             
+        self.gaugeExist = True;
         self.gaugePercent = 0.00
         self.gaugeHeight = 300   
-        self.gaugeWidth = 50    
-        self.gaugeX = 800         
+        self.gaugeWidth = 30    
+        self.gaugeX = 880         
         self.gaugeY = 250 
+        
+        self.imageX = 100
+        self.imageY = 250
+        self.imageW = 0
+        self.imageH = 0
+        
         frameRate(50000)
+    
+    def renderImage(self, content): 
+        self.currentImage = loadImage(content)
     
     def increseGauge(self):
         self.gaugePercent += 0.005
-        if (self.gaugePercent >= 1):
-            self.gaugePercent == 1
+        
+        if self.gaugePercent <= 0.195:
+            self.imageW = 517
+            self.imageH = 81
+            self.renderImage('text4.png')
+        
+        if self.gaugePercent >= 0.2 and self.gaugePercent < 0.395:
+            self.imageW = 342
+            self.imageH = 36
+            self.renderImage('text5.png')
+            
+        elif self.gaugePercent >= 0.4 and self.gaugePercent < 0.595:
+            self.imageW = 571
+            self.imageH = 38
+            
+            self.renderImage('text2.png')
+            
+        elif self.gaugePercent >= 0.6 and self.gaugePercent < 0.795:
+            self.imageW = 252
+            self.imageH = 35
+            self.renderImage('text1.png')
+            
+        elif self.gaugePercent >= 0.8 and self.gaugePercent < 0.995:
+            self.imageW = 569
+            self.imageH = 35
+            self.renderImage('text3.png')
+            
+        if self.gaugePercent >= 0.995:
+            self.gaugePercent == 1.05
+            self.gaugeExist = False
             
         
     def render(self):
@@ -30,21 +66,23 @@ class Page1_1:
         #Page.next()
         colorMode(HSB)
         background(0)
-        
-        
+
         #Draw gauge
-        colorMode(RGB)
-        rectMode(CORNER)  
-        fill(255)
-        noStroke()
-        rect(self.gaugeX, self.gaugeY, self.gaugeWidth, self.gaugeHeight, 10)  
-        fill(100, 100, 200)
-        noStroke()
-        rect(self.gaugeX, self.gaugeY + (1-self.gaugePercent) * self.gaugeHeight, self.gaugeWidth, self.gaugePercent * self.gaugeHeight, 10)
-        textSize(20)
-        textAlign(CENTER, CENTER)
-        fill(0)
-        text(str(int(self.gaugePercent * 100)), self.gaugeX + self.gaugeWidth/2, self.gaugeY + self.gaugeHeight/2)
+        
+        if self.gaugeExist:
+            
+            colorMode(RGB)
+            rectMode(CORNER)  
+            fill(255)
+            noStroke()
+            rect(self.gaugeX, self.gaugeY, self.gaugeWidth, self.gaugeHeight, 10)  
+            fill(255, 255, 0)
+            noStroke()
+            rect(self.gaugeX, self.gaugeY + (1-self.gaugePercent) * self.gaugeHeight, self.gaugeWidth, self.gaugePercent * self.gaugeHeight, 10)
+            textSize(20)
+            textAlign(CENTER, CENTER)
+            fill(52)
+            text(str(int(self.gaugePercent * 100)), self.gaugeX + self.gaugeWidth/2, self.gaugeY + self.gaugeHeight/2)
         
         
         # Draw stars
@@ -53,14 +91,14 @@ class Page1_1:
         strokeWeight(2)
         for s in self.stars:
             point(s.x, s.y)
-            
-        
+
         # Draw fireworks)
         if self.firework.exploded:
             self.firework.update()
         self.firework.show()
         
-        image(self.textImage, 100, 300, 300, 60)
+        image(self.currentImage, self.imageX, self.imageY, self.imageW, self.imageH)
+        
         if self.firework.done():
             firework = None
             Page.next()
